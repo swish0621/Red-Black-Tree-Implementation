19ed69f9229709b9e5090a10d561915cc0a31312
swish0621 <swish0621@gmail.com>
2024-12-08 19:17:14 -0700

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nickswisher/projects/WorkTrack-2
-# Build directory: /Users/nickswisher/projects/WorkTrack-2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest-build")
