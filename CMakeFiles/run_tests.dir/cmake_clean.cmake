a372376113321038c59c4817e04ab6ea773c7d44
swish0621 <swish0621@gmail.com>
2024-12-08 18:50:44 -0700

    Add files via upload

---
+++ CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests.dir/code/WorkTrack.cpp.o"
+  "CMakeFiles/run_tests.dir/code/WorkTrack.cpp.o.d"
+  "run_tests"
+  "run_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
